66f6325fb7f4362fa992625a71326cb39fc5aeb4
shivam-bhushan <shivam13202@gmail.com>
2024-10-20 22:41:02 +0530

    eslint setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    eslint: {
+        // Warning: Disables ESLint checks during production builds
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        // Warning: Ignores TypeScript type errors during production builds
+        ignoreBuildErrors: true,
+    },
+};
 
 export default nextConfig;
